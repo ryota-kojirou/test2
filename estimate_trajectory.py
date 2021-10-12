5106bbd5f36c724063864b6d9ec323b7e5795883
ryota-kojirou <83194733+ryota-kojirou@users.noreply.github.com>
2021-10-12 12:50:04 +0900

    Update estimate_trajectory.py

--- estimate_trajectory.py
+++ estimate_trajectory.py
@@ -151,26 +151,28 @@ def estimate_trajectory(video, path='', resize='432x368', model='cmu', resize_ou
             # plot trajectories r_wrist:4, l_wrist:7
             for hum in np.sort(humans_id):
                 df_human = np_humans[np_humans[:, clm_of_id] == hum]
-                r_start_x = df_human[:, 2 * 3 + 1] * w_pxl
-                r_start_y = df_human[:, 2 * 3 + 2] * h_pxl
-                r_mid_x = df_human[:, 3 * 3 + 1] * w_pxl
-                r_mid_y = df_human[:, 3 * 3 + 2] * h_pxl
-                r_end_x = df_human[:, 4 * 3 + 1] * w_pxl
-                r_end_y = df_human[:, 4 * 3 + 2] * h_pxl
-                r_x_list = [df_human[:, 2 * 3 + 1] * w_pxl, df_human[:, 3 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 1] * w_pxl]
-                r_y_list = [df_human[:, 2 * 3 + 2] * h_pxl, df_human[:, 3 * 3 + 2] * h_pxl, df_human[:, 4 * 3 + 2] * h_pxl]
+                r_start_x = df_human[:, 2 * 3 + 1][-1] * w_pxl
+                r_start_y = df_human[:, 2 * 3 + 2][-1] * h_pxl
+                r_mid_x = df_human[:, 3 * 3 + 1][-1] * w_pxl
+                r_mid_y = df_human[:, 3 * 3 + 2][-1] * h_pxl
+                r_end_x = df_human[:, 4 * 3 + 1][-1] * w_pxl
+                r_end_y = df_human[:, 4 * 3 + 2][-1] * h_pxl
+                r_x_list = [r_start_x, r_mid_x, r_end_x]
+                r_y_list = [r_start_y, r_mid_y, r_end_y]
                 
-                l_start_x = df_human[:, 5 * 3 + 1] * w_pxl
-                l_start_y = df_human[:, 5 * 3 + 2] * h_pxl
-                l_mid_x = df_human[:, 6 * 3 + 1] * w_pxl
-                l_mid_y = df_human[:, 6 * 3 + 2] * h_pxl
-                l_end_x = df_human[:, 7 * 3 + 1] * w_pxl
-                l_end_y = df_human[:, 7 * 3 + 2] * h_pxl
-                r_x_list = [df_human[:, 5 * 3 + 1] * w_pxl, df_human[:, 6 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 1] * w_pxl]
-                r_y_list = [df_human[:, 5 * 3 + 2] * h_pxl, df_human[:, 6 * 3 + 2] * h_pxl, df_human[:, 7 * 3 + 2] * h_pxl]
+                l_start_x = df_human[:, 5 * 3 + 1][-1] * w_pxl
+                l_start_y = df_human[:, 5 * 3 + 2][-1] * h_pxl
+                l_mid_x = df_human[:, 6 * 3 + 1][-1] * w_pxl
+                l_mid_y = df_human[:, 6 * 3 + 2][-1] * h_pxl
+                l_end_x = df_human[:, 7 * 3 + 1][-1] * w_pxl
+                l_end_y = df_human[:, 7 * 3 + 2][-1] * h_pxl
+                l_x_list = [l_start_x, l_mid_x, l_end_x]
+                l_y_list = [l_start_y, l_mid_y, l_end_y]
                 
                 print(df_human[:, 2 * 3 + 1][-1] * w_pxl)
-                plt.plot(df_human[:, 2 * 3 + 1] * w_pxl, df_human[:, 2 * 3 + 2] * h_pxl, linewidth=400/fig_resize)
+                plt.plot(r_x_list, r_y_list, linewidth=400/fig_resize)
+                plt.plot(l_x_list, l_y_list, linewidth=400/fig_resize)
+#                 plt.plot(df_human[:, 2 * 3 + 1] * w_pxl, df_human[:, 2 * 3 + 2] * h_pxl, linewidth=400/fig_resize)
 #                 plt.plot(df_human[:, 4 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 2] * h_pxl, linewidth=400/fig_resize)
 #                 plt.plot(df_human[:, 7 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 2] * h_pxl, linewidth=400/fig_resize)
 

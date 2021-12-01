b98aa3fe3740dc9ad259bb0147da2185785c9bf4
ryota-kojirou <83194733+ryota-kojirou@users.noreply.github.com>
2021-12-02 07:44:06 +0900

    Update estimate_trajectory.py

--- estimate_trajectory.py
+++ estimate_trajectory.py
@@ -154,11 +154,15 @@ def estimate_trajectory(video, path='', resize='432x368', model='cmu', resize_ou
                 if len(df_human)>15:
                   num = len(df_human)-15
                   df_human = df_human[num:]
-#                 for i in range(len(df_human[:, 4 * 3 + 1])):
-#                   plt.plot(df_human[:, 4 * 3 + 1][i] * w_pxl, df_human[:, 4 * 3 + 2][i] * h_pxl, linewidth=400/fig_resize, alpha=0.2+(i/14)*0.5, color="darkorange")
-#                   plt.plot(df_human[:, 7 * 3 + 1][i] * w_pxl, df_human[:, 7 * 3 + 2][i] * h_pxl, linewidth=400/fig_resize, alpha=0.2+(i/14)*0.5, color="darkorange")
-                plt.plot(df_human[:, 4 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 2] * h_pxl, linewidth=400/fig_resize, alpha=0.6, color="darkorange")
-                plt.plot(df_human[:, 7 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 2] * h_pxl, linewidth=400/fig_resize, alpha=0.6, color="darkorange")
+                df_human[:, 4 * 3 + 1] = df_human[:, 4 * 3 + 1] * w_pxl
+                df_human[:, 7 * 3 + 1] = df_human[:, 7 * 3 + 1] * w_pxl  
+                df_human[:, 4 * 3 + 2] = df_human[:, 4 * 3 + 2] * h_pxl
+                df_human[:, 7 * 3 + 2] = df_human[:, 7 * 3 + 2] * h_pxl
+                for i in range(len(df_human[:, 4 * 3 + 1])):
+                  plt.plot(df_human[:, 4 * 3 + 1][i], df_human[:, 4 * 3 + 2], linewidth=400/fig_resize, alpha=0.6, color="darkorange")
+                  plt.plot(df_human[:, 7 * 3 + 1][i], df_human[:, 7 * 3 + 2], linewidth=400/fig_resize, alpha=0.2+(i/14)*0.5, color="darkorange")
+#                 plt.plot(df_human[:, 4 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 2] * h_pxl, linewidth=400/fig_resize, alpha=0.6, color="darkorange")
+#                 plt.plot(df_human[:, 7 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 2] * h_pxl, linewidth=400/fig_resize, alpha=0.6, color="darkorange")
 #                 if count<200:
 # #                   print(df_human)
 # #                   print("\n")

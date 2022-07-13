d71dacb93c14620e9d53935c216dad206c1bec3a
ryota-kojirou <83194733+ryota-kojirou@users.noreply.github.com>
2022-07-14 04:18:03 +0900

    Update trajectory_test.py

--- trajectory_test.py
+++ trajectory_test.py
@@ -8,6 +8,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import csv
 
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
@@ -165,14 +166,11 @@ def estimate_trajectory(video, path='', resize='432x368', model='cmu', resize_ou
         gc.collect()
         if cv2.waitKey(1) == 27:
             break
-    cwd = os.getcwd()
-    print(cwd)
-#     import csv
-#     out_data = [df_human[:, 4 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 2] * h_pxl, df_human[:, 7 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 2] * h_pxl]
-# 　　　　　　　　csv_data = open('write_test.csv', 'w')
-# 　　　　　　　　test = csv.writer(out_data)
-# 　　　　　　　　test.writerows()
-# 　　　　　　　　csv_data.close()        
+    out_data = [df_human[:, 4 * 3 + 1] * w_pxl, df_human[:, 4 * 3 + 2] * h_pxl, df_human[:, 7 * 3 + 1] * w_pxl, df_human[:, 7 * 3 + 2] * h_pxl]
+　　　　　　　　csv_data = open('../drive/My Drive/data/write_test.csv', 'w')
+　　　　　　　　test = csv.writer(out_data)
+　　　　　　　　test.writerows()
+　　　　　　　　csv_data.close()        
     cv2.destroyAllWindows()
     logger.info("finish estimation & start encoding")
     cmd = ["ffmpeg", "-r", str(caps_fps), "-start_number", str(start_frame),
